66d40f664ed2e99c835f254b4e1717a1fee9fe3e
Eunyoung KANG <greenkey20@gmail.com>
2022-12-16 23:27:45 +0900

    fix: update api comment

--- src/main/java/com/homework/todo/ToDoController.java
+++ src/main/java/com/homework/todo/ToDoController.java
@@ -1,9 +1,14 @@
 package com.homework.todo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // 2022.12.16(금) 22h50 프로젝트 생성
+
+/**
+ * ‘To-do Application !’라는 문자열을 출력할 수 있는 간단한 API
+ */
 @RestController
 public class ToDoController {
 
